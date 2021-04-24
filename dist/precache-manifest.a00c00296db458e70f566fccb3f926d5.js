c0c029f8412ceaa47f2e6ce61cb6858a15816c84
gmono <gaozijian0411@foxmail.com>
2021-04-24 19:18:51 +0800

    发布

---
+++ dist/precache-manifest.a00c00296db458e70f566fccb3f926d5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7198481aad7d35f08f07",
+    "url": "/offline-recorder/dist/css/app.a636cc23.css"
+  },
+  {
+    "revision": "f624968d3c5009a13572",
+    "url": "/offline-recorder/dist/css/chunk-vendors.62bee483.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/offline-recorder/dist/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/offline-recorder/dist/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "068ca2b316db98037bebdd1e4f1b9459",
+    "url": "/offline-recorder/dist/fonts/fontello.068ca2b3.ttf"
+  },
+  {
+    "revision": "8d4a4e6f7431d0d7fa92b1df20f38161",
+    "url": "/offline-recorder/dist/fonts/fontello.8d4a4e6f.woff2"
+  },
+  {
+    "revision": "a782baa8633b1359f9686ffad17e0d76",
+    "url": "/offline-recorder/dist/fonts/fontello.a782baa8.woff"
+  },
+  {
+    "revision": "e73a0647198cfe970de0f003be95cc51",
+    "url": "/offline-recorder/dist/fonts/fontello.e73a0647.eot"
+  },
+  {
+    "revision": "9354499c2824248511adf85fdf8e4c37",
+    "url": "/offline-recorder/dist/img/fontello.9354499c.svg"
+  },
+  {
+    "revision": "9ab15ca974bb55b663f57462b9646c5b",
+    "url": "/offline-recorder/dist/index.html"
+  },
+  {
+    "revision": "7198481aad7d35f08f07",
+    "url": "/offline-recorder/dist/js/app.5f26f451.js"
+  },
+  {
+    "revision": "f624968d3c5009a13572",
+    "url": "/offline-recorder/dist/js/chunk-vendors.bed6cd43.js"
+  },
+  {
+    "revision": "b236fc42961dc16025aa236390669948",
+    "url": "/offline-recorder/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/offline-recorder/dist/robots.txt"
+  }
+]);
