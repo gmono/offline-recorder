7d55285371d89c6fa56d9a4c1a69f576fa00136e
gmono <gaozijian0411@foxmail.com>
2021-04-30 10:58:40 +0800

    更新计划

--- src/Main.vue
+++ src/Main.vue
@@ -42,12 +42,12 @@
     <div style="margin-top: 2rem"></div>
     <!-- <router-link to="/new">新录音机</router-link> -->
     <div>
-      <!-- <router-view></router-view> -->
+      <router-view></router-view>
     </div>
-    <van-cell-group>
+    <!-- <van-cell-group>
       <van-cell title="单元格" value="内容"/>
       <van-cell title="单元格" value="内容" label="描述内容"/>
-    </van-cell-group>
+    </van-cell-group> -->
   </div>
 </template>
 
